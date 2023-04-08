f51df0367c69b3d544eb99bcdb2e2fadc7b4cf18
Iori Kojima <iori@iori.dev>
2023-04-08 20:39:05 +0900

    change color

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 8, 85, 48)),
       ),
       home: const FeedPage(title: 'Flutter Demo Home Page'),
     );
